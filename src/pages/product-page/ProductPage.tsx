e33c572f7d081a99314754810caa2f97e9504bf8
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-05-30 23:57:40 +0300

    chore: remove test files (Fri, May 30, 2025 11:57 PM)

--- src/pages/product-page/ProductPage.tsx
+++ src/pages/product-page/ProductPage.tsx
@@ -23,7 +23,6 @@ const ProductPage = () => {
 
   const { id, name, description, masterVariant } = product;
   const images = masterVariant.images || [];
-  // const images = [{ url: '/1.jpg' }, { url: '/2.jpg' }, { url: '/3.jpg' }];
   const price = masterVariant.prices?.[0];
   const regularPrice = price?.value;
   const discountedPrice = price?.discounted?.value;
