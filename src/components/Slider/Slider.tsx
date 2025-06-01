5ba3112a0553d676a5c0904bfa76ad0b31f0b742
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-06-01 23:39:16 +0300

    refactor: extract reusable modal window component (Sun, Jun 01, 2025 11:39 PM)

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,9 +1,10 @@
 import { settings } from './slider-settings';
 import { useState } from 'react';
-import { Box, Fade, Modal } from '@mui/material';
+import { Box } from '@mui/material';
 import { sliderStyles } from './slider-styles';
 import { SliderDetailProps } from '../../types/slider';
 import Slider from 'react-slick';
+import { ModalDetail } from './Modal';
 
 export const SliderDetail = ({ id, name, images }: SliderDetailProps) => {
   const [nav1, setNav1] = useState<Slider | null>(null);
@@ -32,16 +33,9 @@ export const SliderDetail = ({ id, name, images }: SliderDetailProps) => {
           onClick={() => handleOpenModal(0)}
         />
 
-        <Modal open={open} onClose={handleCloseModal} closeAfterTransition>
-          <Fade in={open}>
-            <Box className={css.modal__wrapper} data-testid="modal-wrapper" component="div">
-              <button className={css['slider__close-btn']} onClick={handleCloseModal}>
-                &times;
-              </button>
-              <img className={css.slider__img} src={images[0].url} alt={name.en || name.ru} />
-            </Box>
-          </Fade>
-        </Modal>
+        <ModalDetail open={open} handler={handleCloseModal}>
+          <img className={css.slider__img} src={images[0].url} alt={name.en || name.ru} />
+        </ModalDetail>
       </Box>
     );
   }
@@ -67,35 +61,28 @@ export const SliderDetail = ({ id, name, images }: SliderDetailProps) => {
         ))}
       </Slider>
 
-      <Modal open={open} onClose={handleCloseModal} closeAfterTransition>
-        <Fade in={open}>
-          <Box className={css.modal__wrapper} data-testid="modal-wrapper" component={'div'}>
-            <button className={css['slider__close-btn']} onClick={handleCloseModal}>
-              &times;
-            </button>
-            <Slider
-              key={activeSlide}
-              data-testid="slider"
-              {...settings}
-              asNavFor={nav1 ?? undefined}
-              ref={(slider2): void => setNav2(slider2)}
-              swipeToSlide={true}
-              focusOnSelect={true}
-              initialSlide={activeSlide}
-            >
-              {images.map((img, index) => (
-                <Box key={index}>
-                  <img
-                    className={css.slider__img}
-                    src={img.url}
-                    alt={`${name.en || name.ru} ${index + 1}`}
-                  />
-                </Box>
-              ))}
-            </Slider>
-          </Box>
-        </Fade>
-      </Modal>
+      <ModalDetail open={open} handler={handleCloseModal}>
+        <Slider
+          key={activeSlide}
+          data-testid="slider"
+          {...settings}
+          asNavFor={nav1 ?? undefined}
+          ref={(slider2): void => setNav2(slider2)}
+          swipeToSlide={true}
+          focusOnSelect={true}
+          initialSlide={activeSlide}
+        >
+          {images.map((img, index) => (
+            <Box key={index}>
+              <img
+                className={css.slider__img}
+                src={img.url}
+                alt={`${name.en || name.ru} ${index + 1}`}
+              />
+            </Box>
+          ))}
+        </Slider>
+      </ModalDetail>
     </>
   );
 };
