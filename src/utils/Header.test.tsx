39145bf937daf10e55074af638e7cd739c98eecf
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-06-08 11:43:21 +0300

    test: add test for Header.tsx component (Sun, Jun 08, 2025 11:43 AM)

---
+++ src/utils/Header.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import { AuthProvider } from '../context/context'; // поправь путь под себя
+import { describe, expect, it } from 'vitest';
+import { Path } from '../types/paths';
+
+describe('Header component', () => {
+  it('renders the logo with correct text and link', () => {
+    render(
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </AuthProvider>
+    );
+    const logo = screen.getByText(/🌸 Flower Shop/i);
+    expect(logo).toBeInTheDocument();
+    expect(logo.closest('a')).toHaveAttribute('href', Path.MAIN);
+  });
+});
