6b42f65a94c054cb51ce037a7351487409819b27
Michael5366 <150484080+Michael5366@users.noreply.github.com>
2025-06-17 12:53:22 +0300

    chore: remove comments from loginValidation.ts (Tue, Jun 17, 2025 12:53 PM)

--- src/utils/loginValidation.ts
+++ src/utils/loginValidation.ts
@@ -17,5 +17,4 @@ export const loginValidation = Yup.object({
     .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
     .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
     .matches(/[0-9]/, 'Password must contain at least one digit'),
-  // .matches(/[!@#$%^&*]/, 'Password must contain at least one special character'),
 });
